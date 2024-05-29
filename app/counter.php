af54f00f5793a6e846c96f4243c46be51a809f08
corina <chistolcorina19@gmail.com>
2024-05-29 13:20:57 +0300

    HW img banner

--- app/counter.php
+++ app/counter.php
@@ -1,23 +1,40 @@
 <?
     session_start();
-    var_dump($_SESSION);
+    // var_dump($_SESSION);
 // unset($_SESSION['views']);
 
+//HW1:
+    // if(isset($_SESSION['views'])){
+    //     $_SESSION['views']++;
+    //     print ("You have been here on {$_SESSION['views']} times on the page");
+        
+    // }else{
+    //     $_SESSION['views'] = 1;
+    //     print("First visit");
+    // }
+
+
+
+    // HW2:
+    //   make so the page shows a banner (img ) only the thirst 3 times
     if(isset($_SESSION['views'])){
         $_SESSION['views']++;
-        // print("$views visit");
         print ("You have been here on {$_SESSION['views']} times on the page");
+
+        if($_SESSION['views'] <= 3) {
+            print ("<img src='https://png.pngtree.com/png-vector/20220923/ourmid/pngtree-click-me-3d-button-web-png-image_6213762.png' />"); 
+        }
         
     }else{
+        print ("<img src='https://png.pngtree.com/png-vector/20220923/ourmid/pngtree-click-me-3d-button-web-png-image_6213762.png' />");
         $_SESSION['views'] = 1;
-        // print($_SESSION['views']);
         print("First visit");
     }
+
+
+
+
     
-    
-
-
-
 
     //HW1: 
     //  using hello.php asa example
@@ -28,6 +45,5 @@
     //      4. print you have been here N times on the page), where n = views
     
     
-    // HW2:
-    //   make so the page shows a banner (img ) only the thirst 3 times
+  
 ?>
